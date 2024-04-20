9fa48c573a3fd881d66ec339dbcb63e6c1c98647
Brett Jarvis <brettbjarvis@gmail.com>
2024-04-19 17:26:57 -0700

    set to use CPU instead of GPU, lack of GPU space

--- generate.py
+++ generate.py
@@ -3,12 +3,12 @@ from scipy.io import wavfile
 
 processor = AutoProcessor.from_pretrained("suno/bark")
 model = BarkModel.from_pretrained("suno/bark")
-model.to("cuda")  # Remove this line if you don't have a CUDA-compatible GPU
+# model.to("cuda")  # Remove this line if you don't have a CUDA-compatible GPU
 
 def generate_audio(text, preset, output):
     inputs = processor(text, voice_preset=preset)
-    for k, v in inputs.items():
-        inputs[k] = v.to("cuda")  # Remove this line if you don't have a CUDA-compatible GPU
+    # for k, v in inputs.items():
+        # inputs[k] = v.to("cuda")  # Remove this line if you don't have a CUDA-compatible GPU
     audio_array = model.generate(**inputs)
     audio_array = audio_array.cpu().numpy().squeeze()
     sample_rate = model.generation_config.sample_rate
